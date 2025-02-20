36077de1e3e65070ece5a8085f1834487ef818b8
big-jr <githubcomms@softwarepragmatism.com>
2025-02-20 13:55:52 -0700

    Add sleep and tidying

--- src/api_server/main.py
+++ src/api_server/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from fastapi import FastAPI
@@ -14,17 +15,27 @@ async def root():
 
 @app.get("/sensor/1/data")
 async def temperatures():
+    """
+
+    :return: All of the data from the sensor
+    """
+
+    # Declare the sensor so we can close it later
+    # Maybe use a context manager on this
+    sensor = None
 
     try:
-
         sensor = Amg8833(0x69, 1)
 
-        result = {
+        # Sleep to allow the sensor to settle
+        await asyncio.sleep(0.1)
+
+        return {
             "ambient_temperature": sensor.read_thermistor(),
             "temperature": sensor.read_temp(64),
             "time": time.time(),
         }
 
-        return result
     finally:
-        sensor.close()
+        if sensor:
+            sensor.close()
