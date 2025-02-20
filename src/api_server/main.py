c79d41ceb8b00172232542272ec750b552a8487a
big-jr <githubcomms@softwarepragmatism.com>
2025-02-19 20:58:02 -0700

    Moved folders

---
+++ src/api_server/main.py
@@ -0,0 +1,30 @@
+import time
+
+from fastapi import FastAPI
+
+from sensors.amg8833 import Amg8833
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/sensor/1/data")
+async def temperatures():
+
+    try:
+
+        sensor = Amg8833(0x69, 1)
+
+        result = {
+            "ambient_temperature": sensor.read_thermistor(),
+            "temperature": sensor.read_temp(64),
+            "time": time.time(),
+        }
+
+        return result
+    finally:
+        sensor.close()
