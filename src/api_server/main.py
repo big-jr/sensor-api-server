0e80430724a8b5e8ee12cfa3e9dac8bec594fc3f
big-jr <githubcomms@softwarepragmatism.com>
2025-02-25 21:09:34 -0700

    Use non-cache class

--- src/api_server/main.py
+++ src/api_server/main.py
@@ -3,7 +3,7 @@ import time
 from contextlib import asynccontextmanager
 from typing import Any, Dict
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from src.sensors.amg8833 import Amg8833
 from src.sensors.cached_amg8833 import CachedAmg8833
@@ -16,7 +16,7 @@ async def server_lifespan(app: FastAPI):
 
     # Not a fan of global - tidy this later
     global sensor
-    sensor = CachedAmg8833(0x69, 1)
+    sensor = Amg8833(0x69, 1)
 
     # Sleep to allow the sensor to settle
     await asyncio.sleep(0.2)
@@ -35,12 +35,13 @@ async def root():
 
 
 @app.get("/sensor/0/data")
-async def temperatures():
+async def temperatures(request:Request):
     """
 
     :return: All the data from the sensor
     """
 
+    # cache_control = request.headers.get("cache-control", "")
     error, temperature_readings = sensor.read_temp()
 
     return {
